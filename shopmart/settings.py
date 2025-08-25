66b820928744c83eb14eb992689bc17d199774dd
Stanmartins <miguelstanmartins@gmail.com>
2025-08-25 12:37:20 +0100

    changed the SMTP configuration

--- shopmart/settings.py
+++ shopmart/settings.py
@@ -164,8 +164,8 @@ MESSAGE_TAGS = {
 # EMAIL_USE_TLS = True
 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
