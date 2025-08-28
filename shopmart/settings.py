30398afde8f26357ca111e402c74ef75a2c79902
Stanmartins <miguelstanmartins@gmail.com>
2025-08-28 21:54:55 +0100

    reseted my settings.py and gitignore

--- shopmart/settings.py
+++ shopmart/settings.py
@@ -89,12 +89,12 @@ AUTH_USER_MODEL = 'accounts.Account'  # Set custom user model
 
 DATABASES = {
     'default': {
-        'ENGINE': config('ENGINE', default='django.db.backends.postgresql'),
-        'NAME': config('NAME', default='railway'),
-        "USER": config('USER', default='postgres'),
-        "PASSWORD": config('PASSWORD', default='rzXVbuXwzikeSdyUPomlXRgZZOCVKiFR'),
-        "HOST": config('HOST'),
-        "PORT": config('PORT', cast=int),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "railway",
+        "USER": "postgres",
+        "PASSWORD": "rzXVbuXwzikeSdyUPomlXRgZZOCVKiFR",
+        "HOST": "shinkansen.proxy.rlwy.net",
+        "PORT": "20121"
     }
 }
 
@@ -166,13 +166,13 @@ MESSAGE_TAGS = {
 
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST = config('EMAIL_HOST', default="smtp.gmail.com")
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD =  config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "miguelstanmartins@gmail.com"
+EMAIL_HOST_PASSWORD = "gnnl rpod layl locn"   # Not your Gmail password, use App Passwords
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
