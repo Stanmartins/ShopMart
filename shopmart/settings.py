7a9221f9c007a7326be50de3dcce813eff2abaa9
Stanmartins <miguelstanmartins@gmail.com>
2025-08-28 21:21:43 +0100

    created the gitignore file and also downloaded python-decouple

--- shopmart/settings.py
+++ shopmart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-epvybl(yfem_t4dn@szodj_x2ds&asy3^aa*qe02jfb^p)vcfc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = []
@@ -88,12 +89,12 @@ AUTH_USER_MODEL = 'accounts.Account'  # Set custom user model
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "railway",
-        "USER": "postgres",
-        "PASSWORD": "rzXVbuXwzikeSdyUPomlXRgZZOCVKiFR",
-        "HOST": "shinkansen.proxy.rlwy.net",
-        "PORT": "20121"
+        'ENGINE': config('ENGINE', default='django.db.backends.postgresql'),
+        'NAME': config('NAME', default='railway'),
+        "USER": config('USER', default='postgres'),
+        "PASSWORD": config('PASSWORD'),
+        "HOST": config('HOST'),
+        "PORT": config('PORT', cast=int),
     }
 }
 
@@ -165,13 +166,13 @@ MESSAGE_TAGS = {
 
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "miguelstanmartins@gmail.com"
-EMAIL_HOST_PASSWORD = "gnnl rpod layl locn"   # Not your Gmail password, use App Passwords
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default="smtp.gmail.com")
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =  config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
 
 
 
